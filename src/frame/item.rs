2f59d7c8f480504c4bc7da2b091da65151e618d3
NyanRus <8517501-nyanrus@users.noreply.gitlab.com>
2022-07-21 20:48:12 +0900

    Change Item struct

--- src/frame/item.rs
+++ src/frame/item.rs
@@ -1,8 +1,9 @@
-use std::pin::Pin;
+use std::{cell::RefCell, rc::{Rc, Weak}};
 
 use crate::frame::*;
 
 struct Item {
     ulid:String,
-    vec_child:Vec<Pin<Box<Item>>>,
+    vec_child:Vec<Option<Rc<RefCell<Item>>>>,
+    parent:Option<Weak<RefCell<Item>>>,
 }
